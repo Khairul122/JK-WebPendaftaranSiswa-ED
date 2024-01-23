ba8fa03526a1f50e16576770401d5f5df0d448c3
SarahNelviandri <sarah.210170236@mhs.unimal.ac.id>
2024-01-23 22:07:30 +0700

    first commit

---
+++ view/template/footer.php
@@ -0,0 +1,16 @@
+          </div>
+          <footer class="main-footer">
+            <div class="footer-left">
+              Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
+            </div>
+            <div class="footer-right">
+
+              <!-- Create By <a href="#">Code Peka</a> -->
+            </div>
+          </footer>
+          </div>
+          </div>
+
+          </body>
+
+          </html>
